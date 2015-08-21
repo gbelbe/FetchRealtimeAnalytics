83b6c1d6a12d76d605b3874ade2c2c8dc3b8f580
gaet <gbelbe@gmail.com>
2015-08-21 00:31:51 -0300

    added

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<?php
+// api dependencies
+define('PATH_TO_API', 'google-api/google-api/src/'); 
+
+require_once(PATH_TO_API . 'Google/Client.php');
+require_once(PATH_TO_API . 'Google/Service/Analytics.php');
+
+// create client object and set app name
+$client = new Google_Client();
+$client->setApplicationName(APP_NAME); // name of your app
+
+// set assertion credentials
+$client->setAssertionCredentials(
+  new Google_Auth_AssertionCredentials(
+
+    APP_EMAIL, // email you added to GA
+
+    array('https://www.googleapis.com/auth/analytics.readonly'),
+
+    file_get_contents(PATH_TO_PRIVATE_KEY_FILE)  // keyfile you downloaded
+
+));
+
+// other settings
+$client->setClientId(CLIENT_ID);           // from API console
+$client->setAccessType('offline_access');  // this may be unnecessary?
+
+// create service and get data
+$service = new Google_Service_Analytics($client);
+$service->data_ga->get($ids, $startDate, $endDate, $metrics, $optParams);
+
+php?>
