a5bcd9401a3dfa79677b855bf6ed27e6cd0fd23a
gaet <gbelbe@gmail.com>
2015-08-21 19:14:30 -0300

    test analytics

--- index.php
+++ index.php
@@ -32,46 +32,9 @@ function getService()
   return $analytics;
 }
 
-function getFirstprofileId(&$analytics) {
-  // Get the user's first view (profile) ID.
+function getFirstprofileId(&$analytics) 
 
-  // Get the list of accounts for the authorized user.
-  $accounts = $analytics->management_accounts->listManagementAccounts();
-
-  if (count($accounts->getItems()) > 0) {
-    $items = $accounts->getItems();
-    $firstAccountId = $items[0]->getId();
-
-    // Get the list of properties for the authorized user.
-    $properties = $analytics->management_webproperties
-        ->listManagementWebproperties($firstAccountId);
-
-    if (count($properties->getItems()) > 0) {
-      $items = $properties->getItems();
-      $firstPropertyId = $items[0]->getId();
-
-      // Get the list of views (profiles) for the authorized user.
-      $profiles = $analytics->management_profiles
-          ->listManagementProfiles($firstAccountId, $firstPropertyId);
-
-      if (count($profiles->getItems()) > 0) {
-        $items = $profiles->getItems();
-
-        // Return the first view (profile) ID.
-        return $items[0]->getId();
-
-      } else {
-        throw new Exception('No views (profiles) found for this user.');
-      }
-    } else {
-      throw new Exception('No properties found for this user.');
-    }
-  } else {
-    throw new Exception('No accounts found for this user.');
-  }
-}
-
-function getResults(&$analytics, $profileId) {
+function getResults(&$analytics, 61330176) {
   // Calls the Core Reporting API and queries for the number of sessions
   // for the last seven days.
    return $analytics->data_ga->get(
